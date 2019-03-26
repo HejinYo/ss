ce367917fe2113ce87ff3f7bff3c8ecbe14ac38f
HejinYo <hejinyo@gmail.com>
2019-03-26 23:10:33 +0800

    commit

---
+++ ss-auth/src/main/java/cn/hejinyo/ss/auth/SsAuthApplication.java
@@ -0,0 +1,13 @@
+package cn.hejinyo.ss.auth;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsAuthApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsAuthApplication.class, args);
+    }
+
+}
---
+++ ss-gateway/src/main/java/cn/hejinyo/ss/gateway/SsGatewayApplication.java
@@ -0,0 +1,13 @@
+package cn.hejinyo.ss.gateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsGatewayApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SsGatewayApplication.class, args);
+    }
+
+}
