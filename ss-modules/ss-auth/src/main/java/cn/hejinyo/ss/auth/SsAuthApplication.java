d997a1db067d37b1b00cd883b07f9add7f55122b
HejinYo <hejinyo@gmail.com>
2019-04-14 23:25:30 +0800

    add Swagger2

--- ss-modules/ss-auth/src/main/java/cn/hejinyo/ss/auth/SsAuthApplication.java
+++ ss-modules/ss-auth/src/main/java/cn/hejinyo/ss/auth/SsAuthApplication.java
@@ -2,8 +2,10 @@ package cn.hejinyo.ss.auth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
+@EnableSwagger2
 public class SsAuthApplication {
 
     public static void main(String[] args) {
