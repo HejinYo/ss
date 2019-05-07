836f36a8e51dbb5664a88d3eac658d1adad3031d
anthony.s.he <hejinyo@gmail.com>
2019-05-07 12:40:36 +0800

    认证权限测试初步通过，撒花~ ~ ~

--- ss-common/common-framework/src/main/java/cn/hejinyo/ss/common/framework/exception/ExceptionHandle.java
+++ ss-common/common-framework/src/main/java/cn/hejinyo/ss/common/framework/exception/ExceptionHandle.java
@@ -4,6 +4,7 @@ import cn.hejinyo.ss.common.consts.CommonConstant;
 import cn.hejinyo.ss.common.framework.utils.ResponseUtils;
 import cn.hejinyo.ss.common.framework.utils.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.context.support.DefaultMessageSourceResolvable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.converter.HttpMessageNotReadableException;
 import org.springframework.web.HttpMediaTypeNotSupportedException;
@@ -15,6 +16,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+import java.util.Optional;
 
 /**
  * @author : HejinYo   hejinyo@gmail.com 2017/8/13 19:12
@@ -29,23 +31,19 @@ public class ExceptionHandle {
      */
     @ExceptionHandler(InfoException.class)
     public void infoExceptionException(InfoException e, HttpServletRequest request, HttpServletResponse response) {
-        String uri = request.getRequestURI();
-        String contextPath = request.getContextPath();
-        if ((contextPath + uri).startsWith(CommonConstant.MICRO_SERVER_API)) {
-            // 如果是微服务调用，状态码为500
-            ResponseUtils.response(response, HttpStatus.INTERNAL_SERVER_ERROR.value(), Result.error(e.getCode(), e.getMessage()));
-        } else {
-            ResponseUtils.response(response, HttpStatus.OK.value(), Result.error(e.getCode(), e.getMessage()));
-        }
+        this.handle(request, response, HttpStatus.OK, Result.error(e.getCode(), e.getMessage()));
     }
 
     /**
      * 实体验证
      */
-    @ResponseStatus(HttpStatus.OK)
     @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Result validException(MethodArgumentNotValidException mnve) {
-        return Result.error(mnve.getBindingResult().getFieldError().getDefaultMessage());
+    public void validException(MethodArgumentNotValidException mnve, HttpServletRequest request, HttpServletResponse response) {
+        this.handle(request, response, HttpStatus.UNAUTHORIZED,
+                Result.error(
+                        Optional.ofNullable(mnve.getBindingResult().getFieldError())
+                                .map(DefaultMessageSourceResolvable::getDefaultMessage
+                                ).orElse("实体未通过校验")));
     }
 
     /**
@@ -53,17 +51,29 @@ public class ExceptionHandle {
      */
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     @ExceptionHandler(value = {Exception.class})
-    public Result exception(Exception ex, HttpServletResponse response) {
+    public void exception(Exception ex, HttpServletRequest request, HttpServletResponse response) {
+        Result result;
         if (ex instanceof HttpMessageNotReadableException) {
-            return Result.error(HttpStatus.BAD_REQUEST.getReasonPhrase());
+            result = Result.error(HttpStatus.BAD_REQUEST.getReasonPhrase());
         } else if (ex instanceof HttpRequestMethodNotSupportedException) {
-            return Result.error(HttpStatus.METHOD_NOT_ALLOWED.getReasonPhrase());
+            result = Result.error(HttpStatus.METHOD_NOT_ALLOWED.getReasonPhrase());
         } else if (ex instanceof HttpMediaTypeNotSupportedException) {
-            return Result.error(HttpStatus.UNSUPPORTED_MEDIA_TYPE.getReasonPhrase());
+            result = Result.error(HttpStatus.UNSUPPORTED_MEDIA_TYPE.getReasonPhrase());
         } else {
-            ex.printStackTrace();
+            log.error("系统发生未知错误异常", ex);
+            result = Result.error("未知错误:" + ex.getMessage());
         }
-        log.error("系统发生未知错误异常", ex);
-        return Result.error("未知错误:" + ex.getMessage());
+        this.handle(request, response, HttpStatus.UNAUTHORIZED, result);
+    }
+
+    private void handle(HttpServletRequest request, HttpServletResponse response, HttpStatus httpStatus, Result result) {
+        String uri = request.getRequestURI();
+        String contextPath = request.getContextPath();
+        if ((contextPath + uri).startsWith(CommonConstant.MICRO_SERVER_API)) {
+            // 如果是微服务调用，状态码为500
+            ResponseUtils.response(response, HttpStatus.INTERNAL_SERVER_ERROR.value(), result);
+        } else {
+            ResponseUtils.response(response, httpStatus.value(), result);
+        }
     }
 }
