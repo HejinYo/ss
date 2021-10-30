86bb3b95892426513b4524077127915dfbe6ce2b
贺双双 <hejinyo@gmail.com>
2021-10-30 22:07:03 +0800

    feat:add login.html

--- ss-auth/src/main/java/cn/hejinyo/ss/auth/security/WebSecurityConfig.java
+++ ss-auth/src/main/java/cn/hejinyo/ss/auth/security/WebSecurityConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
@@ -60,4 +61,10 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .and()
                 .formLogin().loginPage("/login");
     }
+
+    @Override
+    public void configure(WebSecurity web) {
+        web.ignoring().antMatchers("/css/**","/img/**");
+    }
+
 }
